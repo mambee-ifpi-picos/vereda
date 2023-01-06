d66f3b87e34d1dd2b91e69d29d453c3a8cf5c06d
Jesiel Viana <jesielviana@proton.me>
2023-01-06 12:45:30 -0300

    config de variaveis de ambiente do firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,10 +7,12 @@ const firebaseConfig = {
   authDomain: 'vereda-app.firebaseapp.com',
   projectId: 'vereda-app',
   storageBucket: 'vereda-app.appspot.com',
-  messagingSenderId: '67717473301',
-  appId: '1:67717473301:web:2385ded2a526d9baac5a1c',
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
   measurementId: 'G-BTY8275BBS',
 }
+
+console.log(' process.env.API_KEY=', firebaseConfig)
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
