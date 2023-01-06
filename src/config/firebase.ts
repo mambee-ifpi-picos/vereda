8d538aeea69bbc9641520c945762285b0ae7ca5d
Jesiel Viana <jesielviana@proton.me>
2023-01-06 19:59:22 -0300

    config env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyByq9ohhLMl8JOeRQTvnbh6vhsbwZUfgP0',
+  apiKey: process.env.API_KEY,
   authDomain: 'vereda-app.firebaseapp.com',
   projectId: 'vereda-app',
   storageBucket: 'vereda-app.appspot.com',
@@ -11,8 +11,6 @@ const firebaseConfig = {
   appId: process.env.APP_ID,
   measurementId: 'G-BTY8275BBS',
 }
-
-console.log(' process.env.API_KEY=', firebaseConfig)
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
